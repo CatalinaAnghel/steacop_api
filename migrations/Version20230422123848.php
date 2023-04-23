10d75b00bd27b99910bd3ab28f4a6f37b553f815
CatalinaAnghel <florina_catalina98@yahoo.com>
2023-04-23 09:50:14 +0300

    Finished the assignments part

---
+++ migrations/Version20230422123848.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230422123848 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE student DROP INDEX IDX_B723AF33166D1F9C, ADD UNIQUE INDEX UNIQ_B723AF33166D1F9C (project_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE student DROP INDEX UNIQ_B723AF33166D1F9C, ADD INDEX IDX_B723AF33166D1F9C (project_id)');
+    }
+}
---
+++ src/Security/Voter/PatchAssignmentVoter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Dto\Assignment\Input\PatchAssignmentInputDto;
+use App\Entity\Assignment;
+use App\Helper\RolesHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class PatchAssignmentVoter extends Voter {
+    public const PATCH = 'PATCH';
+
+    public function __construct(
+        private readonly RequestStack           $requestBody,
+        private readonly EntityManagerInterface $entityManager
+    ) {
+    }
+
+    protected function supports(string $attribute, mixed $subject): bool {
+        return $subject instanceof PatchAssignmentInputDto && $attribute === self::PATCH;
+    }
+
+    /**
+     * @param string $attribute
+     * @param PatchAssignmentInputDto $subject
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        $assignmentId = (int)$this->requestBody->getCurrentRequest()->attributes->get('id');
+        $repo = $this->entityManager->getRepository(Assignment::class);
+        $assignment = $repo->findOneBy(['id' => $assignmentId]);
+
+        if (null === $assignment) {
+            throw new NotFoundHttpException();
+        }
+
+        if (in_array(RolesHelper::RoleTeacher->value, $user->getRoles(), true) &&
+            $assignment->getProject()?->getSupervisor()?->getUser()?->getEmail() === $user->getUserIdentifier()) {
+            return true;
+        }
+
+        $student = $assignment->getProject()?->getStudent();
+        return in_array(RolesHelper::RoleStudent->value, $user->getRoles(), true) &&
+            $student?->getUser()?->getEmail() === $user->getUserIdentifier();
+    }
+}
