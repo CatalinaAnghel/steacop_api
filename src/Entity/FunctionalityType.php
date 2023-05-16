735f240df1670e0c1558174568a0f5e50e7d1071
CatalinaAnghel <florina_catalina98@yahoo.com>
2023-05-16 23:43:49 +0300

    Added endpoints for functionality status and type

--- src/Entity/FunctionalityType.php
+++ src/Entity/FunctionalityType.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FunctionalityTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: FunctionalityTypeRepository::class)]
 class FunctionalityType
 {
